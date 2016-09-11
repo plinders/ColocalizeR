c02df75f0f576402895f71938fc2fd986e7422a5
plinders <plinders@Peters-MBP.lan>
2016-09-11 17:26:51 +0200

    parallel computing file input, moved to data.table instead of data.frame

--- ColocalizeR.R
+++ ColocalizeR.R
@@ -14,6 +14,8 @@ ColocalizeR <- function(dir) {
   require("EBImage")
   require("RBioFormats")
   require("tools")
+  require("data.table")
+  require("parallel")
   
   #Ask for user input to select channels, assume the same values are held per data set
   chan1 <- as.numeric(readline(prompt = "What is the first channel index for analysis? "))
@@ -21,15 +23,19 @@ ColocalizeR <- function(dir) {
   chan2 <- as.numeric(readline(prompt = "What is the second channel index for analysis? "))
   fluor2 <- readline(prompt = "What fluorophore was used for the second channel? ")
   
-  #Read images
+  #Read image file list
   input_files <- dir(pattern = ".oif$|.lif$")
+  
+  #set up parallel computing for image reading optimization
+  total_cores <- detectCores() - 1
+  cl <- makeCluster(total_cores)
   
   #set bools to false so they don't break the script
   oif_bool <- FALSE
   lif_bool <- FALSE
   
   if (sum(grepl(".oif$", input_files)) / length(input_files) == 1) {
-    images_list <- lapply(input_files, read.image)
+    images_list <- parLapply(cl, input_files, read.image)
     oif_meta <- list()
     for (i in seq_along(images_list)) {
       oif_meta[[i]] <- globalMetadata(images_list[[i]])
@@ -46,9 +52,12 @@ ColocalizeR <- function(dir) {
     stop("Only upload .lif or .oif files seperately.")
   }
   
+  #stop cluster to free up memory
+  stopCluster(cl)
+  
   
   #names(images_list) <- paste(input_files, seq_along(images_list))
- 
+  
   #Turn images into matrices, this will become a for loop for automated processing of all files
   #Channel 1 = GFP, Channel 2 = mCh, this could become a user input thing once I figure out how that works
   for (i in seq_along(images_list)) {
@@ -61,32 +70,32 @@ ColocalizeR <- function(dir) {
     
     #Start pdf, plots will go in here
     pdf(paste(folder_name, "_", img_name, ".pdf", sep = ""), paper = "a4")
-
-    chan1_df <- data.frame(getFrame(images_list[[i]], chan1))
-    chan2_df <- data.frame(getFrame(images_list[[i]], chan2))
-  
+    
+    chan1_dt <- data.table(getFrame(images_list[[i]], chan1))
+    chan2_dt <- data.table(getFrame(images_list[[i]], chan2))
+    
     #Change pixel intensity scale from 0 to 1 to 0 to 4096
-    if (max(chan1_df, na.rm = TRUE) <= 1) {
-      chan1_df_16bit <- chan1_df * 4096
-      chan2_df_16bit <- chan2_df * 4096
+    if (max(chan1_dt, na.rm = TRUE) <= 1) {
+      chan1_dt_16bit <- chan1_dt * 4096
+      chan2_dt_16bit <- chan2_dt * 4096
     }
     
     #Plot histograms to show distributions of intensities
     #Data frames cannot be plotted as histograms, hence as.matrix
-    hist(as.matrix(chan1_df_16bit), main = "Pre-normalization", xlab = paste(fluor1, "intensity"))
-    hist(as.matrix(chan2_df_16bit), main = "Pre-normalization", xlab = paste(fluor2, "intensity"))
+    hist(as.matrix(chan1_dt_16bit), main = "Pre-normalization", xlab = paste(fluor1, "intensity"))
+    hist(as.matrix(chan2_dt_16bit), main = "Pre-normalization", xlab = paste(fluor2, "intensity"))
     
     #Scatter plot of un-normalized data
     #Scatter plot takes numeric vectors as input, this line also converts the data frames
-    smoothScatter(as.vector(as.matrix(chan1_df_16bit)), as.vector(as.matrix(chan2_df_16bit)), main = "Pre-normalization", xlab = fluor1, ylab = fluor2)
+    smoothScatter(as.vector(as.matrix(chan1_dt_16bit)), as.vector(as.matrix(chan2_dt_16bit)), main = "Pre-normalization", xlab = fluor1, ylab = fluor2)
     
     #Convert data frames to vectors for further processing
-    chan1_vect <- as.vector(as.matrix(chan1_df_16bit))
-    chan2_vect <- as.vector(as.matrix(chan2_df_16bit))
+    chan1_vect <- as.vector(as.matrix(chan1_dt_16bit))
+    chan2_vect <- as.vector(as.matrix(chan2_dt_16bit))
     
     #Exclude pixels that are < 20% of max in both channels
     chan1_vect[chan1_vect < (0.20 * max(chan1_vect)) & chan2_vect < (0.20 * max(chan2_vect))] <- NA
-
+    
     #Plot new histograms
     hist(chan1_vect, main = "Filter < 20%", xlab = paste(fluor1, "intensity"))
     hist(chan2_vect, main = "Filter < 20%", xlab = paste(fluor2, "intensity"))
@@ -106,12 +115,12 @@ ColocalizeR <- function(dir) {
     chan2_trans <- chan2_relative * (1 / regression_norm$coefficients[1])
     
     #Place both vectors in data frame, plot new scatter plot
-    scatter_df <- data.frame(chan1_relative, chan2_trans)
-    smoothScatter(scatter_df, xlim = c(0, max(scatter_df$chan1_relative, na.rm = TRUE)), ylim = c(0, max(scatter_df$chan2_trans, na.rm = TRUE)), main = "Final", xlab = fluor1, ylab = fluor2)
-    abline(lm(chan2_trans ~ chan1_relative - 1, data = scatter_df), col = "red")
+    scatter_dt <- data.table(chan1_relative, chan2_trans)
+    smoothScatter(scatter_dt, xlim = c(0, max(scatter_dt$chan1_relative, na.rm = TRUE)), ylim = c(0, max(scatter_dt$chan2_trans, na.rm = TRUE)), main = "Final", xlab = fluor1, ylab = fluor2)
+    abline(lm(chan2_trans ~ chan1_relative - 1, data = scatter_dt), col = "red")
     
     #Calculate fineal linear regression
-    final_model <- lm(chan2_trans ~ chan1_relative - 1, data = scatter_df)
+    final_model <- lm(chan2_trans ~ chan1_relative - 1, data = scatter_dt)
     dev.off()
     #Create .txt file to output regression results
     sink(file = paste(folder_name, "_", img_name, ".txt", sep = ""), type = "output")
